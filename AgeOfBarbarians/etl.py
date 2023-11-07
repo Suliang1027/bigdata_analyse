e88113a6b21b418e6ec6fd20dbd1e1f8b1db9d3a
Suliang1027 <sl903dj@hotmail.com>
2023-11-07 17:19:11 +0800

    修改：     AgeOfBarbarians/etl.py 修改：     AgeOfBarbarians/visualize.py

--- AgeOfBarbarians/etl.py
+++ AgeOfBarbarians/etl.py
@@ -12,15 +12,21 @@ from sqlalchemy import create_engine
 
 ############################################# 合并数据文件 ##########################################################
 # 只取用于分析的字段，因为字段数太多，去掉没用的字段可以极大的节省内存和提高效率
-dir = r"C:\Users\Administrator\Desktop\AgeOfBarbarians"
+dir = r"/home/xiaosu/Documents/bigdata_analyse/AgeOfBarbarians"
+################################os.listdir(dir)用于返回指定的文件夹包含的文件或文件夹的名字的列表##########################
+# path =  os.listdir(dir)
+# for file in path:
+#     print(file)
+##################################################################################################################
 data_list = []
 for path in os.listdir(dir):
-    path = os.path.join(dir, path)
+    print(path)
+    path = os.path.join(dir, path) # 把目录和文件名合成一个路径
     data = pd.read_csv(path)
-    data = data[
+    data = pd.DataFrame(data,
         ['user_id', 'register_time', 'pvp_battle_count', 'pvp_lanch_count', 'pvp_win_count', 'pve_battle_count',
          'pve_lanch_count', 'pve_win_count', 'avg_online_minutes', 'pay_price', 'pay_count']
-    ]
+    )
     data_list.append(data)
 data = pd.concat(data_list)
 
@@ -33,5 +39,5 @@ data = pd.concat(data_list)
 
 ############################################# 数据保存 ##########################################################
 # 保存清洗后的数据 mysql
-engine = create_engine('mysql://root:root@172.16.122.25:3306/test?charset=utf8')
+engine = create_engine('mysql+pymysql://root:601500@localhost:3306/test?charset=utf8')
 data.to_sql('age_of_barbarians', con=engine, index=False, if_exists='append')
